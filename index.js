a70ec538895010ca9c89d221e3f8a085336f30c3
Bhusham <bhushan@gmail.com>
2023-05-01 22:43:55 +0530

    check

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const userRoute = require("./route/user.route")
+const connection = require("./db")
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/user", userRoute)
+
+
+app.listen(8080,async()=>{
+    try {
+        await connection
+        console.log("connected to the db")
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log("server is running on port 8080")
+})
